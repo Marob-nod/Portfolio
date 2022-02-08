6bbb395425df420769fcd2bc9d0db52414a585ca
Marob-nod <robin.marien@hotmail.fr>
2022-02-08 16:55:46 +0100

    Add style to the modal

--- new-features/myportfolio/src/App.js
+++ new-features/myportfolio/src/App.js
@@ -7,19 +7,27 @@ import { useState } from 'react';
 
 function App() {
 
-  const projects = ['gsoc', 'lbf', 'rahan', 'ucla', 'search-page', 'cookbook']
+  const projects = [
+    { name: 'gsoc', title: 'GOOGLE SUMMER OF CODE ANALYSER', description: 'Contribution to this awesome tool made for analizing technologies used for the GSoC projects', techs: 'ReactJS, CSS', link: 'https://sparsh1212.github.io/gsocanalyzer/' },
+    { name: 'lbf', title: 'LETS BE FRIENDS', description: 'A Web App to participate or organize events while learning a new language', techs: 'ReactJS, NodeJS, CSS', link: 'https://letsbefriends.netlify.app/' },
+    { name: 'rahan', title: 'RAHAN', description: '', link: '', techs: '' },
+    { name: 'ucla', title: 'CONTRIBUTION TO UNIVERSITY OF LOS ANGELES WEBSITE', description: '', link: '', techs: '' },
+    { name: 'search-page', title: 'LBF - GEOLOCALISTAION AND MULTIPLE FIELDS RESEARCH', description: '', link: '', techs: '' },
+    { name: 'cookbook', title: 'COOKBOOK', description: '', link: '', techs: '' }
+  ]
   const [currentProject, SetCurrentProject] = useState('')
 
   const handleClick = (name) => {
     SetCurrentProject(name)
   }
+
   return (
     <div className="App">
       <Header />
       <main>
         <div className="right">
           {projects.map(project =>
-            <CardProject name={project} currentProject={currentProject} handleClick={handleClick} />
+            <CardProject {...project} currentProject={currentProject} handleClick={handleClick} />
           )}
         </div>
       </main>
--- new-features/myportfolio/src/Components/CardProject/index.js
+++ new-features/myportfolio/src/Components/CardProject/index.js
@@ -1,18 +1,31 @@
 import './styles.scss'
 
 
-function CardProject({ name, handleClick, currentProject }) {
+function CardProject({ name, handleClick, currentProject, title, techs, link, description }) {
 
     return (
         <div className={currentProject === name ? 'item-show' : 'item'} id={name} onClick={() => handleClick(name)} >
-            <div className="vert">{name}</div>
-            <div className="horiz">
-                <span className='name'>{name}</span>
-                <div className="topics">
-                    <strong>100</strong>
-                    <span>TOPICS</span>
-                </div>
-            </div>
+            {currentProject === name ?
+                <div className="banner-top">
+                    <div className='title'>
+                        {title}
+                    </div>
+                    <a className='link' href={link}>
+                        TAKE A LOOK
+                    </a>
+                </div> : ''
+            }
+
+            {currentProject === name ?
+                <div className="banner-bottom">
+                    <div className='description'>
+                        {description.toUpperCase()}
+                    </div>
+                    <a className='techs'>
+                        TECHS: {techs}
+                    </a>
+                </div> : ''
+            }
         </div>
     )
 }
