e98ad3c39118057eb03ba760db66e443dee782f3
Marob-nod <robin.marien@hotmail.fr>
2022-02-08 11:06:15 +0100

    Try to show the whole card

--- new-features/myportfolio/src/App.js
+++ new-features/myportfolio/src/App.js
@@ -1,21 +1,59 @@
-import rahan from './assets/rahan.jpg'
+
 import './App.scss';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CardProject from './Components/CardProject';
+import { useState } from 'react';
 
 function App() {
+
+  const [modals, setModals] = useState([
+    {
+      id: 'gsoc',
+      isClicked: false,
+    },
+    {
+      id: 'lbf',
+      isClicked: false,
+    },
+    {
+      id: 'rahan',
+      isClicked: false,
+    },
+    {
+      id: 'ucla',
+      isClicked: false,
+    },
+    {
+      id: 'search-page',
+      isClicked: false,
+    },
+    {
+      id: 'cookbook',
+      isClicked: false,
+    }
+  ])
+
+  console.log('Je suis dans APP')
+  const handleClick = (id) => {
+    console.log('Début du handleclick')
+    //console.log('event : ', event)
+    console.log('id : ', id)
+    //setModals(modals.map(modal => modal.isClicked = false))
+    const elementClicked = modals.find(modal => modal.id == id)
+    console.log(elementClicked)
+    setModals(elementClicked.isClicked = true)
+    console.log('click de la modal id : ', id, ' et sa valeur bool : ', elementClicked.isClicked)
+  }
   return (
     <div className="App">
       <Header />
       <main>
         <div className="right">
-          <CardProject name="GSoC" id="gsoc" />
-          <CardProject name="LBF" id="lbf" />
-          <CardProject name="Rahan" id="rahan" />
-          <CardProject name="UCLA" id="ucla" />
-          <CardProject name="Search-Page" id="search-page" />
-          <CardProject name="Cookbook" id="cookbook" />
+
+          {modals.map(modal =>
+            <CardProject name={modal.id} id={modal.id} isClicked={modal.isClicked} handleClick={handleClick} />
+          )}
         </div>
       </main>
       <Footer />
--- new-features/myportfolio/src/Components/CardProject/index.js
+++ new-features/myportfolio/src/Components/CardProject/index.js
@@ -1,11 +1,10 @@
 import './styles.scss'
-import react from 'react'
-import { useState } from 'react'
+
 
 function CardProject({ name, id, handleClick, isClicked }) {
 
     return (
-        <div className={isClicked ? 'item-show' : 'item'} id={id} onClick={(id) => handleClick()} >
+        <div className={isClicked ? 'item-show' : 'item'} id={id} onClick={() => handleClick(id)} >
             <div className="vert">{name}</div>
             <div className="horiz">
                 <span className='name'>{name}</span>
